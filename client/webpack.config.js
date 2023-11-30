65b3f1eca96b5518195a2e2d7716905534472cc1
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2023-11-30 00:06:18 -0600

    fixing src-sw.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,47 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+// css webpack
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Text Editor'
+        template: "./index.html",
+        title: "Webpack Plugin",
       }),
-       new MiniCssExtractPlugin(),
+      new MiniCssExtractPlugin(),
       new InjectManifest({
-        swSrc: './src-sw.js', //  source service worker file
-        swDest: 'service-worker.js', // the destination for the output service worker file
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
-      new WebpackPwaManifest({    
-        name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E',
-        inject: true,
+      new WebpackPwaManifest({
         fingerprints: false,
-        description: 'A simple to-do list application',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "offline text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'), // Path to  icon image
-            sizes: [96, 128, 192, 256, 384, 512], // Different sizes for the icon
-            destination: path.join('assets', 'icons'),
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
@@ -48,9 +50,9 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
-         {
+        {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: "asset/resource",
         },
@@ -58,10 +60,13 @@ module.exports = () => {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
